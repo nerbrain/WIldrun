b691f4c5689aaf59fe417705d0599933b1eb92ae
EdwardMuturi <kakaturi300@gmail.com>
2019-01-27 11:22:57 +0300

    Game Over text and score display

---
+++ Assets/Obstacles.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+   private void OnTriggerEnter2D (Collider2D other){
+       if (other.GetComponent<Runner>() != null)
+       {
+           GameController.instance.RunnerScored();
+       }
+   }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class GameController : MonoBehaviour
     public GameObject gameOverText; //used to display game over text 
     public bool gameOver= false;
     public float scrollSpeed= -2.0f; // determines scroll direction and you can override in UI
+    private int score= 0;
+    public Text scoreText;
     
     //called before start and used for initialization
     void Awake()
@@ -17,7 +20,7 @@ public class GameController : MonoBehaviour
         {
             instance = this;
         } else if (instance != this){
-            // Destory (gameObject);
+            // Destory(gameObject);
         }
         
     }
@@ -28,10 +31,18 @@ public class GameController : MonoBehaviour
         
     }
 
-//calls game over screen
-    // public void RunnerDied(){
-    //     gameOverText.setActive(true);
-    //     gamerOver= true;
+    public void RunnerScored(){
+        if(gameOver){
+            return;
+        }
+        score++;
+        scoreText.text= "Score: " + score.ToString();
+    }
 
-    // }
+// calls game over screen
+    public void RunnerDied(){
+        gameOverText.setActive(true);
+        gamerOver= true;
+
+    }
 }
